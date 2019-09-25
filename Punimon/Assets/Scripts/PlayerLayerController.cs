99afe95c4c3952680b79f10997db9bed638eb825
Jan-Hendrik Soechtig <jan-hendrik.soechtig@uni-oldenburg.de>
2019-09-25 19:00:50 +0200

    player controller

---
+++ Punimon/Assets/Scripts/PlayerLayerController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLayerController : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition = new Vector3(0f, 0f, transform.parent.transform.position.y);
+    }
+}
---
+++ Punimon/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed = 5f;
+    private Rigidbody2D _rigidbody;
+    private Vector2 _movement;
+    private Animator _animator;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        if (!_rigidbody) _rigidbody = gameObject.AddComponent<Rigidbody2D>();
+
+        _animator = gameObject.GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _movement.x = Input.GetAxisRaw("Horizontal");
+        _movement.y = Input.GetAxisRaw("Vertical");
+        _animator.SetFloat("Horizontal", _movement.x);
+        _animator.SetFloat("Vertical", _movement.y);
+        _animator.SetFloat("Speed", _movement.magnitude);
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody.MovePosition(_rigidbody.position + Time.fixedDeltaTime * _moveSpeed * _movement);
+    }
+}
